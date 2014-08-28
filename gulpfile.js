60d91ab780e178ebf7c8f7fc4f5f997b38f06528
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-08-28 14:37:17 -0400

    Ignoring node_modules

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var sass = require('gulp-ruby-sass');
+
+gulp.task('default', function() {
+  gulp.src('index.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./'))
+
+  gulp.src('grid.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('./'))
+});
